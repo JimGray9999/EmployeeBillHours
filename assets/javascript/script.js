b158383b9db9cb4e70daf619f535863f19bebc71
Wicked001 <ca55per@gmail.com>
2017-07-12 18:57:04 -0400

    Add files via upload

---
+++ assets/javascript/script.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBuS5HdbUwtidnPxA7CLaA5W18SiMW0Fmw",
+  authDomain: "billable-hours-87d45.firebaseapp.com",
+  databaseURL: "https://billable-hours-87d45.firebaseio.com",
+  projectId: "billable-hours-87d45",
+  storageBucket: "http://billable-hours-87d45.appspot.com",
+  messagingSenderId: "473483339769"
+};
+
+firebase.initializeApp(config);
+
