b224689c8e71e974454de11adb6307852586a191
Jim Gray <JimGray9999@users.noreply.github.com>
2017-07-12 20:00:28 -0400

    updates

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,7 +4,7 @@ var config = {
   authDomain: "billable-hours-87d45.firebaseapp.com",
   databaseURL: "https://billable-hours-87d45.firebaseio.com",
   projectId: "billable-hours-87d45",
-  storageBucket: "http://billable-hours-87d45.appspot.com",
+  storageBucket: "billable-hours-87d45.appspot.com",
   messagingSenderId: "473483339769"
 };
 
@@ -19,10 +19,18 @@ var empRate = "";
 
 
 // on value function
-database.on("value", function(snapshot) {
+database.ref().on("value", function(snapshot) {
 	console.log(snapshot.val());
 });
 
+database.ref().on("child_added", function(snapshot) {
+	console.log("children: ", snapshot.key);
+
+	var sv = snapshot.val();
+
+	$("#empTable").append("<tr>" + sv.empName + "</tr>");
+
+});
 
 // on click function
 //TODO: add the id for the button
@@ -33,10 +41,10 @@ $("#submitBtn").on("click", function() {
 	empStart = $("#startDateForm").val().trim();
 	empRate = $("#rateForm").val().trim();
 
-	database.ref().push(){
+	database.ref().push({
 		empName: empName,
 		empRole: empRole,
 		empStart: empStart,
 		empRate: empRate
-	}
+	});
 });
